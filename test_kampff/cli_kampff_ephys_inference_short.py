3c1730bedf483cc3103c8adc364e5db79f7fc9b6
Ryan Ly <rly@lbl.gov>
2022-01-14 10:58:24 -0800

    Fix paths

--- test_kampff/cli_kampff_ephys_inference_short.py
+++ test_kampff/cli_kampff_ephys_inference_short.py
@@ -11,11 +11,7 @@ if __name__ == '__main__':
     generator_param["pre_post_frame"] = 30
     generator_param["pre_post_omission"] = 1
 
-    generator_param["data_path"] = os.path.join(
-        "/global/cscratch1/sd/rly/deepinterpolation/data/",
-        "c14",
-        "c14_npx_raw.bin"
-    )
+    generator_param["data_path"] = "$SCRATCH/deepinterpolation/data/c14/c14_npx_raw.bin"
     # Note the CLI has changed train_path to data_path to take into account
     # the use of generators for inference
 
@@ -27,13 +23,11 @@ if __name__ == '__main__':
 
     # Replace this path to where you stored your model
     inference_param["model_source"] = {
-        "local_path": "/home/rly/sample_data/2020_02_29_15_28_unet_single_ephys_1024_mean_squared_error-1050.h5"
+        "local_path": "sample_data/2020_02_29_15_28_unet_single_ephys_1024_mean_squared_error-1050.h5"
     }
 
     # Replace this path to where you want to store your output file
-    inference_param[
-        "output_file"
-    ] = "test_kampff/output_short.h5"
+    inference_param["output_file"] = "test_kampff/output_short.h5"
 
     # This option is to add blank frames at the onset and end of the output
     # movie if some output frames are missing input frames to go through
