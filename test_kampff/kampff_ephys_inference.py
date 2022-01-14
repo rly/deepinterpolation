ca7c9c3b2bfa397cbdfc862a2ef260fae2ca5441
Ryan Ly <rly@lbl.gov>
2022-01-14 10:54:51 -0800

    Clean up test script

--- test_kampff/kampff_ephys_inference.py
+++ test_kampff/kampff_ephys_inference.py
@@ -12,11 +12,9 @@ if __name__ == '__main__':
     generator_param["name"] = "KampffEphysGenerator"
     generator_param["pre_post_frame"] = 30
     generator_param["pre_post_omission"] = 1
-    generator_param[
-        "steps_per_epoch"
-    ] = -1
     # No steps necessary for inference as epochs are not relevant.
     # -1 deactivate it.
+    generator_param["steps_per_epoch"] = -1
 
     generator_param["train_path"] = os.path.join(
         # pathlib.Path(__file__).parent.absolute(),
@@ -31,11 +29,10 @@ if __name__ == '__main__':
     generator_param["batch_size"] = 100
     generator_param["start_frame"] = 100
     generator_param["end_frame"] = -1  # -1 to go until the end.
-    generator_param[
-        "randomize"
-    ] = 0
+
     # This is important to keep the order and avoid the
     # randomization used during training
+    generator_param["randomize"] = 0
 
     inferrence_param["type"] = "inferrence"
     inferrence_param["name"] = "core_inferrence"
@@ -43,7 +40,7 @@ if __name__ == '__main__':
     # Replace this path to where you stored your model
     inferrence_param[
         "model_path"
-    ] = "sample_data/2020_02_29_15_28_unet_single_ephys_1024_mean_squared_error-1050.h5"
+    ] = "/home/rly/sample_data/2020_02_29_15_28_unet_single_ephys_1024_mean_squared_error-1050.h5"
 
     # Replace this path to where you want to store your output file
     inferrence_param[
