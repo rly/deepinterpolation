a9f16555c88b803096bef9562c25c32ba9ce0d63
Ryan Ly <rly@lbl.gov>
2022-01-28 17:40:53 -0800

    update scripts

--- sbatch_infer_gpu.sh
+++ sbatch_infer_gpu.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
-#SBATCH -q regular
-#SBATCH -A m3513
-#SBATCH -t 1:59:59
-#SBATCH -n 1
-#SBATCH -o /global/cscratch1/sd/rly/deepinterpolation/test_kampff/infer.%j.log
-#SBATCH -e /global/cscratch1/sd/rly/deepinterpolation/test_kampff/infer.%j.log
-#SBATCH -J kampff_c14
-#SBATCH -C gpu
-#SBATCH -c 10
-#SBATCH --ntasks-per-node 1
-#SBATCH --gpus-per-task 1
+#SBATCH --qos regular
+#SBATCH --constraint gpu
+#SBATCH --account m3513
+#SBATCH --job-name c1
+#SBATCH --output /global/cscratch1/sd/rly/deepinterpolation/sbatch_out/%x.%j.log
+#SBATCH --error /global/cscratch1/sd/rly/deepinterpolation/sbatch_out/%x.%j.log
+#SBATCH --time 3:59:59
+#SBATCH --nodes 1
+#SBATCH --gpus 1
+
+out_sh="/global/cscratch1/sd/rly/deepinterpolation/sbatch_out/$SLURM_JOB_NAME.$SLURM_JOBID.sh"
+me=`basename $0`
+printf "sbatch ${me}\n\n" >> ${out_sh}
+printenv >> ${out_sh}
+printf "\n" >> ${out_sh}
+cat ${me} >> ${out_sh}
+
+out_py="/global/cscratch1/sd/rly/deepinterpolation/sbatch_out/$SLURM_JOB_NAME.$SLURM_JOBID.py"
+cat test_kampff/cli_kampff_ephys_inference.py >> ${out_py}
 
 conda activate /global/cscratch1/sd/rly/env/di
 
+# TODO parameterize
 python test_kampff/cli_kampff_ephys_inference.py
--- test_kampff/cli_kampff_ephys_inference.py
+++ test_kampff/cli_kampff_ephys_inference.py
@@ -3,6 +3,9 @@ from deepinterpolation.cli.inference import Inference
 
 if __name__ == '__main__':
     os.environ['HDF5_USE_FILE_LOCKING'] = 'FALSE'  # necessary for interactive mode on cori
+    # os.environ["CUDA_VISIBLE_DEVICES"] = '-1'  # disable GPU for inference
+    dataset_name = "c1"
+
     generator_param = {}
     inference_param = {}
 
@@ -13,13 +16,13 @@ if __name__ == '__main__':
     generator_param["post_frame"] = 30
     generator_param["pre_post_omission"] = 1
 
-    generator_param["data_path"] = "/global/cscratch1/sd/rly/deepinterpolation/data/c14/c14_npx_raw.bin"
+    generator_param["data_path"] = f"/global/cscratch1/sd/rly/deepinterpolation/data/{dataset_name}/{dataset_name}_npx_raw.bin"
     # Note the CLI has changed train_path to data_path to take into account
     # the use of generators for inference
 
     generator_param["batch_size"] = 100
     generator_param["start_frame"] = 0  # 0 to start at earliest frame
-    generator_param["end_frame"] = 2000000  # -1 to go until the end
+    generator_param["end_frame"] = -1  # -1 to go until the end
     generator_param["randomize"] = False  # should be False for inference
 
     inference_param["name"] = "core_inferrence"
@@ -29,8 +32,12 @@ if __name__ == '__main__':
         "local_path": "sample_data/2020_02_29_15_28_unet_single_ephys_1024_mean_squared_error-1050.h5"
     }
 
+    # Make output directory
+    output_dir = f"/global/cscratch1/sd/rly/deepinterpolation/output/{os.environ['SLURM_JOB_NAME']}.{os.environ['SLURM_JOBID']}/"
+    os.makedirs(output_dir, exist_ok=True)
+
     # Replace this path to where you want to store your output file
-    inference_param["output_file"] = "/global/cscratch1/sd/rly/deepinterpolation/output/c14.h5"
+    inference_param["output_file"] = f"{output_dir}/{dataset_name}.h5"
 
     # This option is to add blank frames at the onset and end of the output
     # movie if some output frames are missing input frames to go through
